020034a44ee50d46ffb29cc54004bfdffbc21e95
lerpaiva <leticiarpaival@gmail.com>
2023-11-16 10:51:13 -0300

    uppp upppp

--- app.py
+++ app.py
@@ -29,20 +29,27 @@ def addTarefas():
     tarefas = tarefas.to_dict('records')        
     return jsonify(tarefas)
 
-@app.route("/update/<string:tarefa_velha>/<string:tarefa_nova>", methods=['PUT'])
-def alterarTarefas(tarefa_velha,tarefa_nova):
-    with open("Text.csv","a") as arquivo:
-        for item in "Text.csv":
-            if item["Tarefa"] == tarefa_velha:
-                att = item["Tarefa"] = tarefa_nova
-                arquivo.write(att)
-                return "Tarefa alterada com sucesso"
-            return "Tarefa nao alterada"   
+@app.route('/update/<string:tarefa_antiga>/<string:tarefa_nova>', methods=['PUT'])
+def update_user(tarefa_antiga, tarefa_nova):
+    tarefas = pd.read_csv('Text.csv')
+
+    for index, row in tarefas.iterrows():
+        if row['TAREFA'] == tarefa_antiga:
+            tarefas.at[index, 'TAREFA'] = tarefa_nova
+            tarefas.to_csv('Text.csv', index=False)
+            return "Tarefa alterada com sucesso"
+    return "Tarefa não encontrada"  
         
-@app.route("/delete/<int:indice_tarefa>", methods=['DELETE']) 
-def deletarTarefas(indice_tarefa):
-    with open("Text.csv","a") as arquivo:
-        pass
+        
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_user(id):
+   tarefas = pd.read_csv('Text.csv')
+   if id in tarefas['ID'].values:
+        tarefas = tarefas[tarefas['ID'] != id]
+        tarefas['ID'] = range(1, len(tarefas) + 1)
+        tarefas.to_csv('Text.csv', index=False)
+        return "Tarefa excluida com sucesso"
+   return "Id não encontrada"
 
 
 if __name__ == '__main__':
