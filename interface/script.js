176ccdc1cdee1f1aef21be513c94f9f4e3bb870a
lerpaiva <leticiarpaival@gmail.com>
2023-11-21 14:15:46 -0300

    upp

--- interface/script.js
+++ interface/script.js
@@ -2,7 +2,7 @@ const tabela = document.querySelector('.tabela-js')
 
 
 
-
+/* Listar*/
 axios.get('http://127.0.0.1:5000/list').then(function(resposta){
     getData(resposta.data)
 }).catch(function(error){
@@ -15,11 +15,12 @@ function getData(data){
         <tr>
             <th scope="row">${item.ID}</th>
             <td>${item.TAREFA}</td>
-            <td><span type="button" class="material-symbols-outlined text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-            delete
-            </span>
+            <td>
             <span type="button" class="material-symbols-outlined text-success" data-bs-toggle="modal" data-bs-target="#exampleModal1">
             edit
+            </span>
+            <span type="button" class="material-symbols-outlined text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+            delete
             </span>
             <td>
         </tr>
@@ -27,3 +28,30 @@ function getData(data){
         })
 
     }
+/* ADICIONAR*/
+const formTarefa = document.getElementById('formTarefa');
+
+formTarefa.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const tarefa = document.getElementById('exampleInputName1').value;
+
+    const Tarefa = {
+        Tarefa: tarefa
+    };
+
+    enviarDadosAPI(Tarefa);
+});
+
+function enviarDadosAPI(Tarefa) {
+    axios.post('http://127.0.0.1:5000/add', Tarefa)
+        .then(response => {
+            console.log(response.data.Tarefa);
+        })
+        .catch(error => {
+            console.error('Erro na requisição POST', error);
+        });
+}
+/*  EXCLUIR*/
+
+
