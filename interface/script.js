60e9ef542efc0874c504e1dc79babd825a9e7ed8
lerpaiva <leticiarpaival@gmail.com>
2023-11-23 09:20:32 -0300

    upp

--- interface/script.js
+++ interface/script.js
@@ -16,10 +16,10 @@ function getData(data){
             <th scope="row">${item.ID}</th>
             <td>${item.TAREFA}</td>
             <td>
-            <span type="button" class="material-symbols-outlined text-success" data-bs-toggle="modal" data-bs-target="#exampleModal1"">
+            <span type="button" class="material-symbols-outlined text-success" data-bs-toggle="modal" data-bs-target="#exampleModal1" onclick="alterar('${item.TAREFA}')">
             edit
             </span>
-            <span type="button" class="material-symbols-outlined text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+            <span type="button" class="material-symbols-outlined text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal2" onclick="excluir(${item.ID})">
             delete
             </span>
             <td>
@@ -54,8 +54,40 @@ function enviarDadosAPI(Tarefa) {
 }
 /*  EXCLUIR*/
 
-function excluir(itemID){
-    axios.delete('http://127.0.0.1:5000/delete/${itemID}')
+function excluir(itemid){
+    const botao = document.getElementById('aa')
+    botao.addEventListener('click', function(){
+        axios.delete(`http://127.0.0.1:5000/delete/${itemid}`)
+           .then(response => {
+                console.log(response.data.Tarefa);
+            })
+            .catch(error => {
+                console.error('Erro na requisição delete', error);
+            });
+})
+} 
 
-}
+/* ALTERAR*/
+function alterar(itemTAREF){
+        const alterarr = document.getElementById('alterarTarefa')
+        alterarr.addEventListener('submit', function(event) {
+            event.preventDefault();
+        
+            const tarefa = document.getElementById('exampleInputName2').value;
+        
+            const Tarefa = {
+                Tarefa: tarefa
+            };
+        
+            enviarDadosAPIATT(itemTAREF,Tarefa);
+        });
 
+function enviarDadosAPIATT(itemTAREF,Tarefa) {
+            axios.put(`http://127.0.0.1:5000/alterar/'${itemTAREF}/${Tarefa}`)
+                .then(response => {
+                    console.log(response.data.Tarefa);
+                })
+                .catch(error => {
+                    console.error('Erro na requisição POST', error);
+                });
+        }}
